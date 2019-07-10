a0830f8579b7ad786a166fb44d99d0527c241fb7
iwtn <ganta.viii@gmail.com>
2019-07-11 04:02:18 +0900

    refactoring?

--- server.rb
+++ server.rb
@@ -5,15 +5,25 @@ require 'digest/sha1'
 OBJECTS_DIR_PATH = './objects'
 MAIN_BRANCH_NAME = 'master'
 
+def resource_path(name)
+  hash = Digest::SHA1.hexdigest(MAIN_BRANCH_NAME)
+  File.join(OBJECTS_DIR_PATH, name, hash)
+end
+
+def dir(name)
+  File.join(OBJECTS_DIR_PATH, name)
+end
+
+def exist_resource?(name)
+  return false unless FileTest.exist?(dir(name))
+
+  FileTest.exist?(resource_path(name))
+end
+
 def read_file(name)
-  dir = File.join(OBJECTS_DIR_PATH, params['name'])
-  raise 'dir not found' unless FileTest.exist?(dir)
+  raise 'resource not found' unless exist_resource?(name)
 
-  hash = Digest::SHA1.hexdigest(MAIN_BRANCH_NAME);
-  path = File.join(OBJECTS_DIR_PATH, params['name'], hash)
-  raise 'file not found' unless FileTest.exist?(path)
-
-  File.open(path) do |f|
+  File.open(resource_path(name)) do |f|
     return f.read
   end
 end
