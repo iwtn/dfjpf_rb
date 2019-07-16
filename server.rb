7725a7dde4e5170b5206fa6efec4cf70ee732a95
iwtn <ganta.viii@gmail.com>
2019-07-17 00:25:43 +0900

    save to a hashed name file

--- server.rb
+++ server.rb
@@ -6,8 +6,12 @@ require 'json'
 OBJECTS_DIR_PATH = './objects'
 MAIN_BRANCH_NAME = 'master'
 
-def resource_path(name)
+def master_path(name)
   hash = Digest::SHA1.hexdigest(MAIN_BRANCH_NAME)
+  File.join(OBJECTS_DIR_PATH, name, hash)
+end
+
+def resource_path(name, hash)
   File.join(OBJECTS_DIR_PATH, name, hash)
 end
 
@@ -18,13 +22,13 @@ end
 def exist_resource?(name)
   return false unless FileTest.exist?(dir(name))
 
-  FileTest.exist?(resource_path(name))
+  FileTest.exist?(master_path(name))
 end
 
 def read_file(name)
   raise 'resource not found' unless exist_resource?(name)
 
-  File.open(resource_path(name)) do |f|
+  File.open(master_path(name)) do |f|
     return f.read
   end
 end
@@ -33,12 +37,13 @@ def add_file(name, key, val)
   raise 'resource not found' unless exist_resource?(name)
 
   json = nil
-  File.open(resource_path(name), 'r') do |f|
+  File.open(master_path(name), 'r') do |f|
     json = JSON.parse(f.read)
   end
   json[key] = val
 
-  File.open(resource_path(name), 'w') do |f|
+  hash = Digest::SHA1.hexdigest(json.to_json)
+  File.open(resource_path(name, hash), 'w') do |f|
     f.write(json.to_json)
   end
 end
